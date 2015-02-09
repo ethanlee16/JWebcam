0ab2c96b132a53a4e46f7dac4273235ffb448c48
Ethan Lee <ethanlee@ethansites.com>
2015-02-09 07:43:07 -0800

    Initial commit

---
+++ src/Filter.java
@@ -0,0 +1,100 @@
+import java.awt.image.*;
+
+public class Filter {
+	
+	private int deltaRed, deltaGreen, deltaBlue;
+	public BufferedImage frame;
+	
+	public Filter() {
+		deltaRed = 0;
+		deltaBlue = 0;
+		deltaGreen = 0;
+	}
+	
+	public Filter(BufferedImage img, int red, int green, int blue) {
+		frame = img;
+		deltaRed = red;
+		deltaGreen = green;
+		deltaBlue = blue;
+	}
+	
+	public void setFrame(BufferedImage nextFrame) {
+		frame = nextFrame;
+	}
+	
+	public void setDeltaRed(int r) {
+		deltaRed = r;
+		System.out.println(deltaRed);
+	}
+	
+	public void setDeltaGreen(int g) {
+		deltaGreen = g;
+	}
+	
+	public void setDeltaBlue(int b) {
+		deltaBlue = b;
+	}
+	
+	public BufferedImage getFrame() {
+		return frame;
+	}
+	
+	public boolean filterAll() {
+		//long time = System.nanoTime();
+		boolean done = false;
+		for (int row = 0; row < frame.getHeight(); row++) { 
+		     for (int col = 0; col < frame.getWidth(); col++) {
+		    	 //filterRed(col, row);
+		    	 //filterBlue(col, row);
+		    	 //filterGreen(col, row);
+		    	 int color = frame.getRGB(col, row);
+		    	 updatePicture(col, row, getRedFromRGB(color) + deltaRed, getGreenFromRGB(color) 
+		    			 + deltaGreen, getBlueFromRGB(color) + deltaBlue);
+		    	// updatePicture(col, row, getRedFromRGB(color) + deltaRed, 0, 0);
+		    	 //System.out.println(frame.getRGB(col, row));
+		     }
+		}
+		done = true;
+		//System.out.println("Elapsed: " + (0.0000000001 * (System.nanoTime() - time)));
+		return done;
+	}
+	
+	public int getRedFromRGB(int rgb) {
+		return (rgb >> 16) & 0xff;
+	}
+	
+	public int getGreenFromRGB(int rgb) {
+		return (rgb >> 8) & 0xff;
+	}
+	
+	public int getBlueFromRGB(int rgb) {
+		return (rgb >> 0) & 0xff;
+	}
+	
+	public void filterRed(int x, int y) {
+		int rgb = frame.getRGB(x, y);
+		int redVal = getRedFromRGB(rgb) + deltaRed;
+		updatePicture(x, y, redVal, getGreenFromRGB(rgb), getBlueFromRGB(rgb));
+	}
+	
+	public void filterGreen(int x, int y) {
+		int rgb = frame.getRGB(x, y);
+		int greenVal = getGreenFromRGB(rgb) + deltaGreen;
+		updatePicture(x, y, getRedFromRGB(rgb), greenVal, getBlueFromRGB(rgb));
+	}
+	
+	public void filterBlue(int x, int y) {
+		int rgb = frame.getRGB(x, y);
+		int blueVal = getBlueFromRGB(rgb) + deltaBlue;
+		updatePicture(x, y, getRedFromRGB(rgb), getGreenFromRGB(rgb), blueVal);
+	}
+	
+	public void updatePicture(int x, int y, int red, int green, int blue) {
+	    // create a 32 bit int with alpha, red, green blue from left to right
+	    int value = (255 << 24) | (red << 16) | (green << 8) | (blue << 0);
+	    //System.out.println("Red: " + red + "\tGreen: "+ green + "\tBlue: " + blue);
+	    // update the picture with the int value
+	    frame.setRGB(x, y, value);
+	}
+	
+}
---
+++ src/Webcam.java
@@ -0,0 +1,92 @@
+ import java.awt.*;  
+ import java.awt.image.BufferedImage;  
+ import java.awt.image.DataBufferByte;  
+ import javax.swing.*;   
+ import org.opencv.core.Mat;   
+ import org.opencv.highgui.VideoCapture;
+ 
+ public class Webcam extends JPanel {
+	 
+      private static final long serialVersionUID = 1L; 
+      
+      private static BufferedImage image;  
+
+      public Webcam(){  
+           super();   
+      }  
+      
+      public static void main(String args[]) {  
+ 		 System.loadLibrary("opencv_java2410");  
+ 		 
+ 		 JFrame frame = new JFrame("Webcam");
+ 		 Webcam panel = new Webcam();
+ 		 
+ 		 // Initialize JPanel parameters
+ 		 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+ 		 frame.setSize(400, 400);
+ 		 frame.setContentPane(panel);       
+ 		 frame.setVisible(true);
+ 		 
+ 		 Mat currentImage = new Mat();
+ 		 Filter boost = new Filter(image, 0, 0, 0);
+
+ 		 
+ 		 // VideoCapture and Mat are classes from OpenCV
+ 		 VideoCapture capture = new VideoCapture(0);   
+ 		 if(capture.isOpened()) {
+ 			 // Infinitely update the images
+ 			 while(true) {
+ 				 // VideoCapture returns current Mat
+ 				 capture.read(currentImage);
+ 				 if(!currentImage.empty()) {   
+ 					 frame.setSize(currentImage.width() + 40, currentImage.height() + 60);   
+ 					 image = panel.matrixToBuffer(currentImage);
+ 					 boost.setFrame(image);
+ 					 
+ 					// TRIPPY
+ 					 boost.setDeltaRed((int) (Math.random() * 255));
+ 					 boost.setDeltaGreen((int) (Math.random() * 255));
+ 					 boost.setDeltaBlue((int) (Math.random() * 255));
+ 					 
+ 					 boost.filterAll();
+ 			    	 image = boost.getFrame();
+ 			    	 
+ 					 // Update the panel
+ 					 panel.repaint();
+ 				 }  
+ 				 else {   
+ 					 System.out.println("Error: no frame captured");
+ 					 break;
+ 				 }  
+ 			 }  
+ 		 }  
+ 		 return;  
+ 	 }  
+      /**  
+       * Converts/writes a Mat into a BufferedImage.  
+       *   
+       * @param matrix Mat of type CV_8UC3 or CV_8UC1  
+       * @return BufferedImage of type TYPE_3BYTE_BGR or TYPE_BYTE_GRAY  
+       */  
+      public BufferedImage matrixToBuffer(Mat matBGR){  
+           int width = matBGR.width(), height = matBGR.height(), channels = matBGR.channels();
+           byte[] sourcePixels = new byte[width * height * channels];
+           matBGR.get(0, 0, sourcePixels);
+           // create new image and get reference to backing data
+           image = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+           final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+           System.arraycopy(sourcePixels, 0, targetPixels, 0, sourcePixels.length);
+           //image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+           return image;
+      }
+      public void paintComponent(Graphics g){
+    	  super.paintComponent(g);   
+    	  if (image == null) {
+    		  return;  
+    	  }
+    	 
+    	  g.drawImage(image, 10, 10, image.getWidth(), image.getHeight(), null);
+    	  g.setColor(Color.WHITE);
+    	  g.drawString("JWebcam v1.0 by Ethan Lee", 30, 40);  
+      }  
+ }  
